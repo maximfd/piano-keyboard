5052f09db86bcad1893b7fbbde7b0f41710c6dd5
maximfd <m.fedenyov@gmail.com>
2022-02-01 14:15:30 +0300

    Add keyboard controls for the piano

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
+const keysControls = {
+    'z' : 'C', 
+    's' : 'Db',
+    'x' : 'D',
+    'd' : 'Eb', 
+    'c' : 'E',
+    'v' : 'F',
+    'g' : 'Gb', 
+    'b' : 'G',
+    'h' : 'Ab', 
+    'n' : 'A', 
+    'j' : 'Bb',
+    'm' : 'B'
+}
+
 const keyboardKeys = document.querySelectorAll('.keyboard__key')
 
 keyboardKeys.forEach(key => {
     key.addEventListener('click', () => playAudio(key))
+})
+
+document.addEventListener('keydown', e => {
+    if (e.repeat) return
+    
+    const note = keysControls[e.key]
+    keyboardKeys.forEach(key => {
+        if (key.dataset.note === note) playAudio(key);
+    })
 })
 
 function playAudio(key) {
